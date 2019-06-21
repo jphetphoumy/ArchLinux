589b04150ff79150d583cb2185916c67cf3271fe
jphetphoumy <j.phetphoumy@gmail.com>
2019-06-21 09:30:12 +0000

    Upadted bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 # Automate ArchLinux Install
 
+timedatectl set-ntp true
 hostname=$(dialog --stdout --inputbox "Set hostname Hostname:" 0 0) || exit 1
 : ${hostname:?"hostname cannot be empty"}
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|loop|rpmb" | tac)
@@ -9,4 +10,16 @@ device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) 
 parted --script "${device}" -- mklabel gpt \
 	mkpart ESP fat32 1Mib 129MiB \
 	set 1 boot on \
-	mkpart primary ext4 2177MiB 100%
+	mkpart primary ext4 129MiB 100%
+
+part_boot="${device}1"
+part_root="${device}2"
+
+mkfs.fat -F32 ${part_boot}
+mkfs.ext4 ${part_root}
+mount ${part_root} /mnt
+mkdir /mnt/efi
+mount ${part_boot} /mnt/boot
+pacstrap /mnt base
+genfstab -U /mnt >> /mnt/etc/fstab
+
