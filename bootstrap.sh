2decea037e302176f5c7a5a5949f341e87b14677
Jeremy Phetphoumy <jeremy.phetphoumy@openevents.fr>
2020-01-08 18:42:10 +0100

    Update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,30 +3,36 @@
 # Automate ArchLinux Install
 
 timedatectl set-ntp true
+
+# Try to umount /mnt if already mounted
+umount -R /mnt
+
+# Get Hostname set by user
 hostname=$(dialog --stdout --inputbox "Set hostname Hostname:" 0 0) || exit 1
 : ${hostname:?"hostname cannot be empty"}
+
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|loop|rpmb" | tac)
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
-parted --script "${device}" -- mklabel gpt \
-	mkpart ESP fat32 1Mib 129MiB \
-	set 1 boot on \
-	mkpart primary ext4 129MiB 100%
 
-part_boot="${device}1"
+# Partition disk
+parted --script ${device} mklabel gpt \
+	mkpart primary fat32 1Mib 261MiB \
+	set 1 esp on \
+	mkpart primary ext4 261MiB 100%
+
+# Define partition
+part_efi="${device}1"
 part_root="${device}2"
-wipefs ${part_boot}
-wipefs ${part_root}
 
-mkfs.fat -F32 ${part_boot}
+mkfs.fat -F32 ${part_efi}
 mkfs.ext4 ${part_root}
+
 mount ${part_root} /mnt
-mkdir /mnt/boot/efi -p 
-mount -t vfat ${part_boot} /mnt/boot/efi
-mkdir /mnt/boot/efi/EFI -p 
-grub-install --target=x86_64-efi --boot-directory=/mnt/boot --efi-directory=/mnt/boot/efi --bootloader-id=ArchLinux --recheck
-pacstrap /mnt base
+
+mount ${part_efi} /mnt/efi
+
+pacstrap /mnt base linux linux-firmware git ansible efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt pacman -S grub efibootmgr git ansible vim --noconfirm
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 arch-chroot /mnt hwclock --systohc
 arch-chroot /mnt locale-gen
@@ -35,4 +41,5 @@ arch-chroot /mnt echo KEYMAP=fr-latin9 > /etc/vconsole.conf
 arch-chroot /mnt echo $hostname > /etc/hostname
 arch-chroot /mnt echo "127.0.1.1 ${hostname}.localdomain ${hostname}" >> /etc/hosts
 arch-chroot /mnt echo "root:changeme" | chpasswd
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
