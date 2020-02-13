4842bf5ec803d50acf4d95c6cb8f0b1f2cb93d01
Jeremy Phetphoumy <j.phetphoumy@gmail.com>
2020-02-13 15:41:33 +0100

    Update bootstrap, added efibootmgr

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ mkdir /mnt/efi -p
 mount /dev/sda1 /mnt/efi
 
 # Installation
-pacstrap /mnt base linux linux-firmware git vim
+pacstrap /mnt base linux linux-firmware git vim grub efibootmgr
 
 # Settin fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -31,3 +31,9 @@ arch-chroot /mnt sed -i 's/#\(fr_FR\.UTF-8\)/\1/' /etc/locale.gen
 arch-chroot /mnt locale-gen
 arch-chroot /mnt echo "KEYMAP=fr-latin9"
 arch-chroot /mnt echo "Devbox" > /etc/hostname
+arch-chroot /mnt echo "127.0.0.1	localhost" >> /etc/hosts
+arch-chroot /mnt echo "::1	localhost" >> /etc/hosts
+arch-chroot /mnt echo "127.0.1.1	Devbox.localdomain	Devbox" >> /etc/hosts
+arch-chroot /mnt echo "root:changeme" | chpasswd
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
